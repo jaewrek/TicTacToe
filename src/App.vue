876da14ec5864af5be78e45aaf2e33b7f4114d98
Jerrik <jerrik@sendlane.com>
2018-05-11 20:07:04 -0700

    Project Submission

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    
+      <span class="title">TIC TAC TOE</span>
+      <Board/>
+    
+  </div>
+</template>
+
+<script>
+import Board from './components/Board'
+
+export default {
+  name: 'App',
+  components: {
+    Board
+  },
+}
+</script>
+
+<style>
+body {
+  background-color: #fff;
+  color: #fff;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin: 0px;
+}
+#app {
+  margin: 0 auto;
+  max-width: 475px;
+}
+.title {
+  font-family: 'Titan One', Helvetica, sans-serif;
+  color: black;
+  font-size: 3em;
+}
+</style>
